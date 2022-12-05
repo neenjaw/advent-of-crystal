52d46851ff42ba372af1f9f29a7b5e884c354895
Tim Austin <tim@neenjaw.com>
2022-12-05 00:12:04 -0600

    day 5

---
+++ src/advent_of_crystal/Year2022/Day05/solution.cr
@@ -0,0 +1,64 @@
+input_file = ARGV[0]
+puts "📖 Reading #{input_file}:"
+
+# File.each_line(input_file) do |line|
+#   input << line
+# end
+
+puzzle_in, moves_in = File.read(input_file).chomp.split(/\n\n/).map(&.split(/\n/))
+
+# pp puzzle_in
+
+class Puzzle
+  def initialize(input)
+    @stacks = {} of Int32 => Array(Char)
+
+    input.reverse[1..].each do |line|
+      line.chars.in_groups_of(4).each_with_index do |group, i|
+        _, c = group
+        push(i + 1, c) unless !c || c == ' '
+      end
+    end
+  end
+
+  def push(col : Int32, value : Char)
+    @stacks[col] ||= [] of Char
+    @stacks[col].unshift(value)
+  end
+
+  def move(from : Int32, to : Int32, count : Int32)
+    count.times { do_move(from, to) }
+  end
+
+  private def do_move(from, to)
+    value = @stacks[from].shift
+    @stacks[to] ||= [] of Char
+    @stacks[to].unshift(value)
+  end
+
+  def move_many(from : Int32, to : Int32, count : Int32)
+    values = @stacks[from].shift(count)
+    @stacks[to] ||= [] of Char
+    values.reverse.each { |v| @stacks[to].unshift(v) }
+  end
+
+  def get_tops
+    @stacks.keys.sort.map { |k| @stacks[k][0] }.compact.join
+  end
+end
+
+part1 = moves_in.reduce(Puzzle.new(puzzle_in)) do |puzzle, move|
+  move.match(/^move (\d+) from (\d+) to (\d+)$/)
+  puzzle.move($~[2].to_i, $~[3].to_i, $~[1].to_i)
+  puzzle
+end.get_tops
+
+pp part1
+
+part2 = moves_in.reduce(Puzzle.new(puzzle_in)) do |puzzle, move|
+  move.match(/^move (\d+) from (\d+) to (\d+)$/)
+  puzzle.move_many($~[2].to_i, $~[3].to_i, $~[1].to_i)
+  puzzle
+end.get_tops
+
+pp part2
